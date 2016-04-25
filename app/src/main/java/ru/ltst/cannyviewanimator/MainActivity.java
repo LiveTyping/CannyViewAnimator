24b29f5c2f4dd242b618f0c1747929109c079205
Danil Perevalov <tzarsuperman@yandex.ru>
2016-04-25 21:36:38 +0600

    Create project

---
+++ app/src/main/java/ru/ltst/cannyviewanimator/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.ltst.cannyviewanimator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}

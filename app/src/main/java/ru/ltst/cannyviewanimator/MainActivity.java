f0fc4d905ac3409f92fd411a197a191c895ab3ae
Danil Perevalov <tzarsuperman@yandex.ru>
2016-04-29 12:59:39 +0600

    Refactoring. Remove Butterknife. Update sample

--- app/src/main/java/ru/ltst/cannyviewanimator/MainActivity.java
+++ app/src/main/java/ru/ltst/cannyviewanimator/MainActivity.java
@@ -1,40 +1,41 @@
 package ru.ltst.cannyviewanimator;
 
 import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
-import android.support.v7.app.AppCompatActivity;
+import android.animation.PropertyValuesHolder;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import java.util.List;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 import ru.ltst.library.CannyViewAnimator;
 
-public class MainActivity extends AppCompatActivity implements CannyViewAnimator.InOutAnimators{
+public class MainActivity extends AppCompatActivity implements CannyViewAnimator.Animators,
+        View.OnClickListener {
 
-    @Bind(R.id.main_animator)
-    protected CannyViewAnimator animator;
-
-    private float width;
+    private CannyViewAnimator animator;
+    private int buttons[] = new int[]{
+            R.id.main_first_button,
+            R.id.main_second_button,
+            R.id.main_third_button};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-        ButterKnife.bind(this);
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        width = displayMetrics.widthPixels;
-        animator.setInOutAnimators(this);
+        initViews();
     }
 
-    @OnClick({R.id.main_first_button, R.id.main_second_button, R.id.main_third_button})
-    protected void onButtonClick(View view){
+    private void initViews() {
+        animator = (CannyViewAnimator) findViewById(R.id.main_animator);
+        if (animator != null) animator.setAnimators(this);
+        for (int id : buttons) {
+            View view = findViewById(id);
+            if (view != null) view.setOnClickListener(this);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
         switch (view.getId()) {
             case R.id.main_first_button:
                 animator.setDisplayedChildId(R.id.main_first_view);
@@ -50,27 +51,15 @@ public class MainActivity extends AppCompatActivity implements CannyViewAnimator
 
     @Override
     public Animator getInAnimator(View inChild, View outChild) {
-        ObjectAnimator in = ObjectAnimator.ofFloat(null, View.TRANSLATION_X, -width, 0);
-        in.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                View view = (View) ((ObjectAnimator) animation).getTarget();
-                view.setTranslationX(0);
-            }
-        });
-        return in;
+        PropertyValuesHolder scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y.getName(), 0.4f, 1);
+        PropertyValuesHolder scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X.getName(), 0.4f, 1);
+        return ObjectAnimator.ofPropertyValuesHolder(inChild, scaleX, scaleY);
     }
 
     @Override
     public Animator getOutAnimator(View inChild, View outChild) {
-        ObjectAnimator out = ObjectAnimator.ofFloat(null, View.TRANSLATION_X, 0, width);
-        out.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                View view = (View) ((ObjectAnimator) animation).getTarget();
-                view.setTranslationX(0);
-            }
-        });
-        return out;
+        PropertyValuesHolder scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y.getName(), 1, 0.4f);
+        PropertyValuesHolder scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X.getName(), 1, 0.4f);
+        return ObjectAnimator.ofPropertyValuesHolder(outChild, scaleX, scaleY);
     }
 }

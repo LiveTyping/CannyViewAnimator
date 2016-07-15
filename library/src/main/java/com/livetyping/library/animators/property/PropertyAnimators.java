2a70b0075875d14c8bf9680f3a034503d11a53ca
Danil Perevalov <tzarsuperman@yandex.ru>
2016-07-16 01:17:35 +0600

    Add default rotation animators

--- library/src/main/java/com/livetyping/library/animators/property/PropertyAnimators.java
+++ library/src/main/java/com/livetyping/library/animators/property/PropertyAnimators.java
@@ -18,8 +18,28 @@ public enum PropertyAnimators implements DefaultCannyAnimators {
     SCALE_X(new PropertyIn(View.SCALE_X, 0, 1), new PropertyOut(View.SCALE_X, 1, 0)),
     SCALE_X_HALF(new PropertyIn(View.SCALE_X, 0.5f, 1), new PropertyOut(View.SCALE_X, 1, 0.5f)),
     SCALE_Y(new PropertyIn(View.SCALE_Y, 0, 1), new PropertyOut(View.SCALE_Y, 1, 0)),
-    SCALE_Y_HALF(new PropertyIn(View.SCALE_Y, 0.5f, 1), new PropertyOut(View.SCALE_Y, 1, 0.5f));
-
+    SCALE_Y_HALF(new PropertyIn(View.SCALE_Y, 0.5f, 1), new PropertyOut(View.SCALE_Y, 1, 0.5f)),
+    //rotation
+    ROTATION_90(new PropertyIn(View.ROTATION, 90, 0), new PropertyOut(View.ROTATION, 0, 90)),
+    ROTATION_180(new PropertyIn(View.ROTATION, 180, 0), new PropertyOut(View.ROTATION, 0, 180)),
+    ROTATION_360(new PropertyIn(View.ROTATION, 360, 0), new PropertyOut(View.ROTATION, 0, 360)),
+    ROTATION_M90(new PropertyIn(View.ROTATION, -90, 0), new PropertyOut(View.ROTATION, 0, -90)),
+    ROTATION_M180(new PropertyIn(View.ROTATION, -180, 0), new PropertyOut(View.ROTATION, 0, -180)),
+    ROTATION_M360(new PropertyIn(View.ROTATION, -360, 0), new PropertyOut(View.ROTATION, 0, -360)),
+    //rotation x
+    ROTATION_X_90(new PropertyIn(View.ROTATION_X, 90, 0), new PropertyOut(View.ROTATION_X, 0, 90)),
+    ROTATION_X_180(new PropertyIn(View.ROTATION_X, 180, 0), new PropertyOut(View.ROTATION_X, 0, 180)),
+    ROTATION_X_360(new PropertyIn(View.ROTATION_X, 360, 0), new PropertyOut(View.ROTATION_X, 0, 360)),
+    ROTATION_X_M90(new PropertyIn(View.ROTATION_X, -90, 0), new PropertyOut(View.ROTATION_X, 0, -90)),
+    ROTATION_X_M180(new PropertyIn(View.ROTATION_X, -180, 0), new PropertyOut(View.ROTATION_X, 0, -180)),
+    ROTATION_X_M360(new PropertyIn(View.ROTATION_X, -360, 0), new PropertyOut(View.ROTATION_X, 0, -360)),
+    //rotation y
+    ROTATION_Y_90(new PropertyIn(View.ROTATION_Y, 90, 0), new PropertyOut(View.ROTATION_Y, 0, 90)),
+    ROTATION_Y_180(new PropertyIn(View.ROTATION_Y, 180, 0), new PropertyOut(View.ROTATION_Y, 0, 180)),
+    ROTATION_Y_360(new PropertyIn(View.ROTATION_Y, 360, 0), new PropertyOut(View.ROTATION_Y, 0, 360)),
+    ROTATION_Y_M90(new PropertyIn(View.ROTATION_Y, -90, 0), new PropertyOut(View.ROTATION_Y, 0, -90)),
+    ROTATION_Y_M180(new PropertyIn(View.ROTATION_Y, -180, 0), new PropertyOut(View.ROTATION_Y, 0, -180)),
+    ROTATION_Y_M360(new PropertyIn(View.ROTATION_Y, -360, 0), new PropertyOut(View.ROTATION_Y, 0, -360));
     private InAnimator inAnimator;
     private OutAnimator outAnimator;
 
